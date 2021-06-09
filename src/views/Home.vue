ac9e791d667c5463a2cb3e3eda8af15cce62870d
guangyan <jingguangyan@offcn.com>
2021-06-09 18:18:59 +0800

    chore: add alias demo

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <div>{{count}}</div>
+      <button @click="count++">click me</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/composition-api'
+export default {
+  name: 'Home',
+  setup () {
+    const count = ref(0)
+    return {
+      count
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.home {
+  height: 100%;
+  .container {
+    width: 100%;
+    min-height: 100%;
+    background: #f0f2f5 url(~@/assets/background.svg) no-repeat 50%;
+    background-size: 100%;
+    //padding: 50px 0 84px;
+    position: relative;
+  }
+}
+
+</style>
